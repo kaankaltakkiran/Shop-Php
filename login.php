4694eab8e8f76edc1ed821c7b87a1e89a29f90a9
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2023-11-20 21:46:14 +0300

    fix login system

--- login.php
+++ login.php
@@ -1,3 +1,6 @@
+<?php
+$activePage="login";
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -5,53 +8,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
   </head>
   <body>
-    <?php
+  <?php
     require_once('navbar.php');
     ?>
-    <div class="container">
-  <div class="row justify-content-center mt-3">
-  <div class="col-6">
-
-<form method="POST">
-<h1 class="text-center text-danger">Login</h1>
-  <div class="form-floating mb-3">
-  <input type="email" name="form_email" class="form-control">
-  <label>Email</label>
-</div>
-<div class="form-floating mb-3">
-  <input type="password" name="form_password"class="form-control">
-  <label>Password</label>
-</div>
-
-                  <button type="submit" name="submit" class="btn btn-primary">Login</button>   	
-     </form>
-     </div>
-</div>
-</div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  </body>
-</html>
 <?php
-$connect = mysqli_connect("localhost", "root", "root", "shop");
-$connect->set_charset("utf8mb4");
+require_once('db.php');
 
 @session_start();
 
-// Eğer zaten giriş yapmışsa, index.php'ye yönlendir
+//! Eğer zaten giriş yapmışsa, index.php'ye yönlendir
 if (isset($_SESSION['isLogin'])) {
   // Oturum açmış
   header("location: index.php");
   die();
 }
-
+//!form_email post edilmişse
 if (isset($_POST['form_email'])) {
   // Form gönderildi
   // 1.DB'na bağlan
   // 2.SQL hazırla ve çalıştır
   // 3.Gelen sonuç 1 satırsa GİRİŞ BAŞARILI değilse, BAŞARISIZ
-
+  //! Eğer boş alan varsa uyarı mesajı
   if(empty($_POST["form_email"]) || empty($_POST["form_password"]))  
   {  
        echo '
@@ -63,35 +43,47 @@ if (isset($_POST['form_email'])) {
                   </div>
                   </div>
                   ';    
-  }  
+  }
+  //! Boş alan yoksa  
   else  
-  {  
-       $useremail = mysqli_real_escape_string($connect, $_POST["form_email"]);  
-       $userpassword = mysqli_real_escape_string($connect, $_POST["form_password"]);  
-       $query = "SELECT * FROM users WHERE useremail = '$useremail'";  
-       $result = mysqli_query($connect, $query);  
-       if(mysqli_num_rows($result) > 0)  
-       {  
-            while($row = mysqli_fetch_array($result))  
-            {  
-                 if(password_verify($userpassword, $row["userpassword"]))  
+  {
+    //! Post edilen verileri değişkenlere atama
+    $useremail  = $_POST['form_email'];
+    $userpassword = $_POST['form_password']; 
+    //! SQL hazırlama ve çalıştırma
+    //! formdan gelen email ile db de varsa
+    $sql = "SELECT * FROM users  WHERE useremail = :form_email";
+    $SORGU = $DB->prepare($sql);
+  
+    $SORGU->bindParam(':form_email',$useremail);
+  
+    $SORGU->execute();
+  
+    $CEVAP = $SORGU->fetchAll(PDO::FETCH_ASSOC);
+      /* var_dump($CEVAP);
+      echo "Gelen cevap " .  count($CEVAP) . " adet satırdan oluşuyor";
+      die(); */
+      //! Gelen sonuç 1 satırsa db de kullanıcı var olduğunu anlarız
+       if(count($CEVAP) == 1)  
+       { 
+                  //! Kullanıcının şifresini doğrulama
+                  //? posttan gelen ile db den gelen karşılaştırma
+                  //? password_verify() fonksiyonu ile   
+                 if(password_verify($userpassword, $CEVAP[0]['userpassword']))  
                  {  
                       //return true;  
-                     //echo "<h1>GİRİŞ BAŞARILI!</h1>";
-                     //Session başlatma
                      @session_start();
                      $_SESSION['isLogin'] = 1; // Kullanıcı giriş yapmışsa 1 yap
-                     $_SESSION['adsoyad'] = $row['username']; // Kullanıcının adını al
-                     $_SESSION['id'] = $row['userid']; // Kullanıcının ID'sini al
-                     $_SESSION['rol'] = $row['role']; // Kullanıcının ROL'ünü al
+                     $_SESSION['adsoyad'] = $CEVAP[0]['username']; // Kullanıcının adını al
+                     $_SESSION['id'] = $CEVAP[0]['userid']; // Kullanıcının ID'sini al
+                     $_SESSION['rol'] = $CEVAP[0]['role']; // Kullanıcının ROL'ünü al
                     header("location: index.php");
                     die();    
                  }  
                  else  
                  {  
                       //return false;  
-                      //echo $userpassword;
-                      //echo $row["password"];
+                      //!Şifreler Eşleşmiyorsa
                       echo '
                       <div class="container">
                       
@@ -101,14 +93,14 @@ if (isset($_POST['form_email'])) {
                   </div>
                   </div>
                   ';  
-                 }  
+                  
             }  
        }   
        else  
-       {  
+       {
+        //! Kullanıcı yoksa  
         echo '
-        <div class="container">
-        
+        <div class="container">  
     <div class="alert mt-3 text-center alert-danger alert-dismissible fade show" role="alert">
     INCORRECT EMAIL or PASSWORD!...
       <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
@@ -120,3 +112,31 @@ if (isset($_POST['form_email'])) {
   
 }
 ?>
+    <div class="container">
+  <div class="row justify-content-center mt-3">
+  <div class="col-6">
+
+<form method="POST">
+<h1 class="text-center text-danger">Login</h1>
+  <div class="form-floating mb-3">
+  <input type="email" name="form_email" class="form-control">
+  <label>Email</label>
+</div>
+<!-- <div class="form-floating mb-3">
+  <input type="password" name="form_password"class="form-control">
+  <label>Password</label>
+</div> -->
+<div class="input-group mb-3  input-group-lg">
+  <input type="password"  name="form_password" class="form-control" id="password" placeholder="Password">
+  <span class="input-group-text bg-transparent"><i id="togglePassword" class="bi bi-eye-slash"></i></span>
+</div>
+
+                  <button type="submit" name="submit" class="btn btn-primary">Login</button>   	
+     </form>
+     </div>
+</div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="hideShow.js"></script>
+  </body>
+</html>
--- register.php
+++ register.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Register</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
   </head>
   <body>
     <?php
@@ -21,16 +22,16 @@ if (isset($_POST['form_email'])) {
 <form method="POST">
 <h1 class="text-center text-danger">Register</h1>
 <div class="form-floating mb-3">
-  <input type="text" name="form_name" class="form-control">
+  <input type="text" name="form_name" class="form-control"required>
   <label>Name</label>
 </div>
   <div class="form-floating mb-3">
-  <input type="email" name="form_email" class="form-control">
+  <input type="email" name="form_email" class="form-control"required>
   <label>Email</label>
 </div>
-<div class="form-floating mb-3">
-  <input type="password" name="form_password"class="form-control">
-  <label>Password</label>
+<div class="input-group mb-3  input-group-lg">
+  <input type="password"  name="form_password" class="form-control" id="password" placeholder="Password"required>
+  <span class="input-group-text bg-transparent"><i id="togglePassword" class="bi bi-eye-slash"></i></span>
 </div>
 
                   <button type="submit" name="submit" class="btn btn-primary">Register</button>   	
@@ -39,6 +40,7 @@ if (isset($_POST['form_email'])) {
 </div>
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="hideShow.js"></script>
   </body>
 </html>
 <?php
