7e48693fa9e328116940da96efa40176351c0f41
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2023-11-21 09:48:33 +0300

    add example image

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
 $username   = "root";
-$password   = "root";
-$dbname     = "shop";
+$password   = "";
+$dbname     = "";
 
 try {
   $DB = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
