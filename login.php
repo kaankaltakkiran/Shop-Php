8c4170ff0e59c231031ae186e14037194520d0d3
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2023-12-28 15:30:39 +0300

    added login page user information

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-$activePage="login";
+$activePage = "login";
 ?>
 <!doctype html>
 <html lang="en">
@@ -12,104 +12,96 @@ $activePage="login";
   </head>
   <body>
   <?php
-    require_once('navbar.php');
-    ?>
+require_once 'navbar.php';
+?>
 <?php
-require_once('db.php');
+require_once 'db.php';
 
 @session_start();
 
 //! Eğer zaten giriş yapmışsa, index.php'ye yönlendir
 if (isset($_SESSION['isLogin'])) {
-  // Oturum açmış
-  header("location: index.php");
-  die();
+    // Oturum açmış
+    header("location: index.php");
+    die();
 }
 //!form_email post edilmişse
 if (isset($_POST['form_email'])) {
-  // Form gönderildi
-  // 1.DB'na bağlan
-  // 2.SQL hazırla ve çalıştır
-  // 3.Gelen sonuç 1 satırsa GİRİŞ BAŞARILI değilse, BAŞARISIZ
-  //! Eğer boş alan varsa uyarı mesajı
-  if(empty($_POST["form_email"]) || empty($_POST["form_password"]))  
-  {  
-       echo '
+    // Form gönderildi
+    // 1.DB'na bağlan
+    // 2.SQL hazırla ve çalıştır
+    // 3.Gelen sonuç 1 satırsa GİRİŞ BAŞARILI değilse, BAŞARISIZ
+    //! Eğer boş alan varsa uyarı mesajı
+    if (empty($_POST["form_email"]) || empty($_POST["form_password"])) {
+        echo '
                       <div class="container">
-                      
+
                   <div class="alert mt-3 text-center alert-info alert-dismissible fade show" role="alert">
                   Both Fields are required...
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                   </div>
                   </div>
-                  ';    
-  }
-  //! Boş alan yoksa  
-  else  
-  {
-    //! Post edilen verileri değişkenlere atama
-    $useremail  = $_POST['form_email'];
-    $userpassword = $_POST['form_password']; 
-    //! SQL hazırlama ve çalıştırma
-    //! formdan gelen email ile db de varsa
-    $sql = "SELECT * FROM users  WHERE useremail = :form_email";
-    $SORGU = $DB->prepare($sql);
-  
-    $SORGU->bindParam(':form_email',$useremail);
-  
-    $SORGU->execute();
-  
-    $CEVAP = $SORGU->fetchAll(PDO::FETCH_ASSOC);
-      /* var_dump($CEVAP);
-      echo "Gelen cevap " .  count($CEVAP) . " adet satırdan oluşuyor";
-      die(); */
-      //! Gelen sonuç 1 satırsa db de kullanıcı var olduğunu anlarız
-       if(count($CEVAP) == 1)  
-       { 
-                  //! Kullanıcının şifresini doğrulama
-                  //? posttan gelen ile db den gelen karşılaştırma
-                  //? password_verify() fonksiyonu ile   
-                 if(password_verify($userpassword, $CEVAP[0]['userpassword']))  
-                 {  
-                      //return true;  
-                     @session_start();
-                     $_SESSION['isLogin'] = 1; // Kullanıcı giriş yapmışsa 1 yap
-                     $_SESSION['adsoyad'] = $CEVAP[0]['username']; // Kullanıcının adını al
-                     $_SESSION['id'] = $CEVAP[0]['userid']; // Kullanıcının ID'sini al
-                     $_SESSION['rol'] = $CEVAP[0]['role']; // Kullanıcının ROL'ünü al
-                    header("location: index.php");
-                    die();    
-                 }  
-                 else  
-                 {  
-                      //return false;  
-                      //!Şifreler Eşleşmiyorsa
-                      echo '
+                  ';
+    }
+    //! Boş alan yoksa
+    else {
+        //! Post edilen verileri değişkenlere atama
+        $useremail = $_POST['form_email'];
+        $userpassword = $_POST['form_password'];
+        //! SQL hazırlama ve çalıştırma
+        //! formdan gelen email ile db de varsa
+        $sql = "SELECT * FROM users  WHERE useremail = :form_email";
+        $SORGU = $DB->prepare($sql);
+
+        $SORGU->bindParam(':form_email', $useremail);
+
+        $SORGU->execute();
+
+        $CEVAP = $SORGU->fetchAll(PDO::FETCH_ASSOC);
+        /* var_dump($CEVAP);
+        echo "Gelen cevap " .  count($CEVAP) . " adet satırdan oluşuyor";
+        die(); */
+        //! Gelen sonuç 1 satırsa db de kullanıcı var olduğunu anlarız
+        if (count($CEVAP) == 1) {
+            //! Kullanıcının şifresini doğrulama
+            //? posttan gelen ile db den gelen karşılaştırma
+            //? password_verify() fonksiyonu ile
+            if (password_verify($userpassword, $CEVAP[0]['userpassword'])) {
+                //return true;
+                @session_start();
+                $_SESSION['isLogin'] = 1; // Kullanıcı giriş yapmışsa 1 yap
+                $_SESSION['adsoyad'] = $CEVAP[0]['username']; // Kullanıcının adını al
+                $_SESSION['id'] = $CEVAP[0]['userid']; // Kullanıcının ID'sini al
+                $_SESSION['rol'] = $CEVAP[0]['role']; // Kullanıcının ROL'ünü al
+                header("location: index.php");
+                die();
+            } else {
+                //return false;
+                //!Şifreler Eşleşmiyorsa
+                echo '
                       <div class="container">
-                      
+
                   <div class="alert mt-3 text-center alert-danger alert-dismissible fade show" role="alert">
                   INCORRECT EMAIL or PASSWORD MATCH!...
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                   </div>
                   </div>
-                  ';  
-                  
-            }  
-       }   
-       else  
-       {
-        //! Kullanıcı yoksa  
-        echo '
-        <div class="container">  
+                  ';
+
+            }
+        } else {
+            //! Kullanıcı yoksa
+            echo '
+        <div class="container">
     <div class="alert mt-3 text-center alert-danger alert-dismissible fade show" role="alert">
     INCORRECT EMAIL or PASSWORD!...
       <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
     </div>
     ';
-       }  
-  }  
-  
+        }
+    }
+
 }
 ?>
     <div class="container">
@@ -131,9 +123,35 @@ if (isset($_POST['form_email'])) {
   <span class="input-group-text bg-transparent"><i id="togglePassword" class="bi bi-eye-slash"></i></span>
 </div>
 
-                  <button type="submit" name="submit" class="btn btn-primary">Login</button>   	
+                  <button type="submit" name="submit" class="btn btn-primary">Login</button>
      </form>
      </div>
+</div>
+<div class="row justify-content-end ">
+  <div class="col-6 ">
+  <h3 class="text-center text-info">User İnformation</h3>
+<table class="table  table-striped">
+  <thead>
+    <tr>
+    <th scope="col">#</th>
+      <th scope="col">Email</th>
+      <th scope="col">Password</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>ahmet@gmail.com</td>
+      <td>123</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>admin@gmail.com</td>
+      <td>admin</td>
+    </tr>
+  </tbody>
+</table>
+</div>
 </div>
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
